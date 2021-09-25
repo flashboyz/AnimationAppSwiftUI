6af49272d5f9b1d47c333ddab13f2bf5b7b9e3bb
Konstantin Prokofiev <flashboyz@yandex.ru>
2021-09-25 12:09:51 +0300

    Initial Commit

---
+++ AnimationAppSwiftUI/AnimationAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationAppSwiftUIApp.swift
+//  AnimationAppSwiftUI
+//
+//  Created by Константин Прокофьев on 25.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
